58cd1b8b73f25d101ac75a2f3953d7314ffba29c
Cassel Josue <cjosue@ga-f1.21-school.ru>
2019-09-20 16:16:56 +0300

    20.09.2019

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -21,13 +21,9 @@ static void			ft_strncut(char **dst, size_t i, int fd)
 	char			*tmp;
 
 	len = ft_strlen(dst[fd]);
-//	printf("%s ____cut\n", dst[fd]);
-//	printf("%zu __len\n", len);
-	tmp = ft_strsub(dst[fd], i + 1 , len - i);
+	tmp = ft_strsub(dst[fd], i + 1, len - i);
 	ft_strdel(&dst[fd]);
-//	printf("%s ____tmp\n", tmp);
 	dst[fd] = tmp;
-//	printf("%s ___ cut\n", dst[fd]);
 }
 
 static void			ft_pr_strjoin(char **dst, char *src, int fd)
@@ -35,78 +31,58 @@ static void			ft_pr_strjoin(char **dst, char *src, int fd)
 	char			*tmp;
 
 	tmp = ft_strjoin(dst[fd], src);
-//	printf("%s\n", tmp);
 	ft_strdel(&dst[fd]);
-//	printf("%s  __tmp\n", tmp);
 	dst[fd] = tmp;
-//	printf("%s  __dst\n", dst[fd]);
 }
 
 static int			ft_read(char **dst, const int fd)
 {
 	char			buf[BUFF_SIZE + 1];
 	int				ret;
-	int				yes;
-	int				i;
 
-	yes = 0;
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
 		buf[ret] = '\0';
-		i = 0;
-/*		while (buf[i] && yes != 1)
-		{
-			if (buf[i++] == '\n')
-				yes = 1;
-		} */
 		ft_pr_strjoin(dst, buf, fd);
+		if (ft_strchr(buf, '\n'))
+			break ;
 	}
 	if (ret < 0)
 		return (-1);
-//	printf("%s ___read\n", dst[fd]);
-//	if (ret == 0 )
-//		ft_pr_strjoin(dst, buf);
-	return (yes);
+	return (ret > 0);
 }
 
-static int			ft_writing(char **dst, char **line, int fd)
+static int			ft_writing(char **dst, char **line, int fd, int ret)
 {
 	size_t			i;
-	int				yes;
 
 	i = 0;
-	yes = 0;
 	while (dst[fd][i] && dst[fd][i] != '\n')
 		i++;
-	if (dst[fd][i] && dst[fd][i] == '\n')
-		yes = 1;
-	if(!(*line = ft_strsub(dst[fd], 0, i)))
+	if (ret == 0 && (dst[fd] == NULL || dst[fd][0] == '\0'))
+		return (0);
+	if (!(*line = ft_strsub(dst[fd], 0, i)))
 	{
 		ft_strdel(&dst[fd]);
-		ft_strdel(&*line);
 		return (-1);
 	}
-	ft_strncut(dst, i , fd);
-	return (yes);
+	ft_strncut(dst, i, fd);
+	return (1);
 }
 
 int					get_next_line(const int fd, char **line)
 {
 	static	char	*fd_buf[255];
+	int				ret;
 
-	//	if (!fd_buf || fd_buf == NULL)
-//		fd_buf = (char**)malloc(sizeof(char**) * 256);
 	if (!line || fd < 0)
 		return (-1);
 	if (!fd_buf[fd])
-		fd_buf[fd] = ft_strnew(1);
-
-	if ( (ft_read(fd_buf, fd)) < 0)
+		fd_buf[fd] = ft_strnew(0);
+	if ((ret = ft_read(fd_buf, fd)) < 0)
 	{
 		ft_strdel(&fd_buf[fd]);
 		return (-1);
 	}
-//	printf("%s ____fd_buf\n", fd_buf[fd]);
-	return (ft_writing(fd_buf, line, fd));
-//	return (0);
+	return (ft_writing(fd_buf, line, fd, ret));
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -12,12 +12,11 @@
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
 # include <stdlib.h>
 # include <fcntl.h>
-
-# define BUFF_SIZE 128
+# define BUFF_SIZE 8
 
 int		get_next_line(const int fd, char **line);
 
 #endif
-
